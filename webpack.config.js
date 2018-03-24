39d520a0b63db3607a254c09c3bae4668bed9d5f
Sergey Zhirkov <aralot@mail.ru>
2018-03-24 21:59:10 +0300

    New build

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,7 @@ module.exports = {
         filename: '[name].js',
         library: 'scratch-gui',
         libraryTarget: 'umd',
-        publicPath: (process.env.NODE_ENV === 'production') ?
-            'singlepage/scratch-gui-media/' : ''
+        publicPath: 'singlepage/scratch-gui-media/'
     },
     externals: {
         React: 'react',
